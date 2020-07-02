9f8f16bb7d08dffedaad3bcbf68a65f0f61fb9bd
Little-Green-Guy <54310886+Little-Green-Guy@users.noreply.github.com>
2020-07-02 10:31:05 +0100

    move r9k function to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const config = require("./config.json")
+const fs  = require('fs')
 
 var nlFilterArray = []
 
@@ -10,8 +11,49 @@ client.on("ready", () => {
 	client.user.setActivity("a-help")
 })
 
+//these files need to be created manually
+const lastMessages = require("./messages.json") // blank file should be just []
+const r9kMutes = require("./mutes.json") // blank file should be just {}
+
+setInterval(decay, 21600000) // run decay function every 6 hours
+
+function decay(){ // loop through all id's and half their mute duration
+	for (var id in r9kMutes){
+		r9kMutes[id][0] = Math.floor(r9kMutes[id][0]/2)
+	}
+	fs.writeFileSync('mutes.json', JSON.stringify(r9kMutes))
+}
+
 client.on("message", async message => {
 	if(message.author.bot) return
+	
+	//MESSAGE IN R9K CHANNEL
+	
+	if(message.channel.id === config.r9kChannelID){
+		//apply primary filter - make all lowercase and remove non alphanumberic characters
+		const r9kMessage = message.content.toLowerCase().replace(/[_\W]+/g, "")
+
+		//check if author of message is currently muted
+		if(message.author.id in r9kMutes && r9kMutes[message.author.id][1] > Date.now()){
+			message.delete()
+			return
+		}
+
+		//check if message is an infraction
+		if(lastMessages.includes(r9kMessage)){
+			message.delete()
+			//either quadruple previous time or start with 1 (in seconds)
+			const muteDuration = message.author.id in r9kMutes ? r9kMutes[message.author.id][0]*4 : 1
+			const muteUntil = Date.now() + 1000*muteDuration
+			r9kMutes[message.author.id] = [muteDuration, muteUntil] // add/update their mute variables
+			fs.writeFileSync('mutes.json', JSON.stringify(r9kMutes))
+			message.reply("you have been muted for " + muteDuration + " seconds")
+		}
+
+		lastMessages.push(r9kMessage)  // add message to log if message was successful
+		fs.writeFileSync('messages.json', JSON.stringify(lastMessages))
+		return
+	}
 	
 	//COMMANDS WITHOUT PREFIX
 
@@ -88,7 +130,7 @@ client.on("message", async message => {
 
 	if(command === "anon" || command === "a"){
 		const anonMessage = args.join(" ")
-		message.delete().catch(O_o=>{})
+		//message.delete().catch(O_o=>{}) // wtf is this and why is it here
 
 		//Newline filter (max 5)
 		nlre = /\n/g
